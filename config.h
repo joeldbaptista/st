10f6476b4043f1b2f6ce0872791b8aca2cee01e3
joel <jdbaptista@pm.me>
2026-04-11 14:45:46 +0100

    remove unnecessary lines

--- config.h
+++ config.h
@@ -5,9 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-//static char *font = "hack:pixelsize=14:antialias=true:autohint=true";
-static char *font = "terminus:pixelsize=20:antialias=true:autohint=true";
+static char *font = "hack:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 6;
 
 /*
